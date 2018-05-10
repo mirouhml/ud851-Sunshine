9b284c2e11052529770ef1e5e494c1b19f7e19f0
mirouhml <amarhamlaoui@gmail.com>
2018-05-10 16:19:00 +0100

    Gradle update

---
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/NetworkRequest.java
@@ -0,0 +1,19 @@
+package com.example.android.sunshine;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Phoenix on 5/9/2018.
+ */
+
+public class NetworkRequest extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+}
